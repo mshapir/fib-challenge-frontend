b5589b53973df9cd9610da7242c55255d9bd6844
mshapir <mshapir95@gmail.com>
2020-02-18 00:14:06 -0500

    added secure deployed endpoint

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   handleSubmit = (event) => {
     this.setState({sequence: ""})
     event.preventDefault()
-    fetch(`http://fib-api-mshapir.herokuapp.com/fibs/fib_result/${this.state.user_input}/${this.state.isPrime}`)
+    fetch(`https://fib-api-mshapir.herokuapp.com/fibs/fib_result/${this.state.user_input}/${this.state.isPrime}`)
       .then((r) => {
       return r.json();
     })
